c9d3c7d08dfe6a236f2f73e67d6e9c471a5c8407
신승헌 <shshin030601@gmail.com>
2025-07-05 00:58:58 +0900

    ft_memchr done

---
+++ ft_memchr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sinseungheon <sinseungheon@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/04 22:26:34 by sinseungheo       #+#    #+#             */
+/*   Updated: 2025/07/05 00:57:36 by sinseungheo      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+	char 	*s1;
+
+	i = 0;
+	s1 = (char*)s;
+	while (i < n && s1[i] != 0)
+	{
+		if (s1[i] == c)
+		{
+			return (s1 + i);
+		}
+		i++;
+	}
+	return (0);
+}
+/*
+#include <string.h>
+#include <stdio.h>
+int main(void) {
+    char str[] = "hell world";
+    char *p = ft_memchr(str, 'o', strlen(str));
+
+    if (p != NULL) {
+        printf("'o' found at position: %ld\n", p - str);
+    } else {
+        printf("'o' not found.\n");
+    }
+    return 0;
+}
+*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sinseungheon <sinseungheon@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 23:27:36 by sinseungheo       #+#    #+#             */
-/*   Updated: 2025/07/04 01:36:41 by sinseungheo      ###   ########.fr       */
+/*   Updated: 2025/07/05 00:58:23 by sinseungheo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int		toupper(int c);
 int		tolower(int c);
 char	*strchr(const char *s, int c);
 char	*strrchr(const char *s, int c);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif                //위의 #ifndef 조건의 끝을 표시
