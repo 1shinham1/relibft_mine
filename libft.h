790e97d0a2627a5975a39edefc6ec307d419bd72
신승헌 <shshin030601@gmail.com>
2025-07-16 16:55:42 +0900

    ft_striteri done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sinseungheon <sinseungheon@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 23:27:36 by sinseungheo       #+#    #+#             */
-/*   Updated: 2025/07/16 16:19:36 by sinseungheo      ###   ########.fr       */
+/*   Updated: 2025/07/16 16:50:52 by sinseungheo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 #endif                //위의 #ifndef 조건의 끝을 표시
 /*
